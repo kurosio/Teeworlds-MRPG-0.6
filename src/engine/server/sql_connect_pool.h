a008467312561054195667ceeeadeb56f85b878b
kurosio <harborwolf1@gmail.com>
2022-09-30 20:41:44 +0700

    unused

--- src/engine/server/sql_connect_pool.h
+++ src/engine/server/sql_connect_pool.h
@@ -36,18 +36,6 @@ enum class DB
 	(output) = buffer;                          \
 }
 
-static int va_str_format(char* buffer, int buffer_size, const char* format, va_list VarArgs)
-{
-#if defined(CONF_FAMILY_WINDOWS)
-	_vsnprintf(buffer, buffer_size, format, VarArgs);
-	buffer[buffer_size - 1] = 0;
-#else
-	vsnprintf(buffer, buffer_size, format, VarArgs);
-	/* null termination is assured by definition of vsnprintf */
-#endif
-	return str_utf8_fix_truncation(buffer);
-}
-
 class CConectionPool
 {
 	CConectionPool();
