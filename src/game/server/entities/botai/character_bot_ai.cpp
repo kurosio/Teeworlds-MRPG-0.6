219e15c55d40dba38dbe02465d0efea9fc3dc284
kurosio <harborwolf1@gmail.com>
2023-08-07 21:14:28 +0700

    add point's to orbite for quest npc bot

--- src/game/server/entities/botai/character_bot_ai.cpp
+++ src/game/server/entities/botai/character_bot_ai.cpp
@@ -51,7 +51,7 @@ bool CCharacterBotAI::Spawn(class CPlayer *pPlayer, vec2 Pos)
 	}
 	else if(m_pBotPlayer->GetBotType() == TYPE_BOT_QUEST && QuestBotInfo::ms_aQuestBot[MobID].m_HasAction)
 	{
-		GS()->CreateLaserOrbite(ClientID, 3, EntLaserOrbiteType::MOVE_RIGHT, 0.15f, 76.f);
+		GS()->CreateLaserOrbite(ClientID, 5, EntLaserOrbiteType::MOVE_RIGHT, 0.15f, 64.f);
 	}
 	else if(m_pBotPlayer->GetBotType() == TYPE_BOT_NPC)
 	{
