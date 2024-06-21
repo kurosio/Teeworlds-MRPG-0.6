eb7cc1f1539132ee7311db7ba70569cacc87c739
kurosio <harborwolf1@gmail.com>
2024-06-21 15:09:42 +0700

    fix optional equipped item for bots

--- src/game/server/playerbot.cpp
+++ src/game/server/playerbot.cpp
@@ -522,7 +522,10 @@ void CPlayerBot::GetFormatedName(char* aBuffer, int BufferSize)
 std::optional<int> CPlayerBot::GetEquippedItemID(ItemFunctional EquipID, int SkipItemID) const
 {
 	if((EquipID >= EQUIP_HAMMER && EquipID <= EQUIP_LASER) || EquipID == EQUIP_ARMOR)
-		return DataBotInfo::ms_aDataBot[m_BotID].m_aEquipSlot[EquipID];
+	{
+		int itemID = DataBotInfo::ms_aDataBot[m_BotID].m_aEquipSlot[EquipID];
+		return (itemID > 0) ? std::make_optional(itemID) : std::nullopt;
+	}
 	return std::nullopt;
 }
 
