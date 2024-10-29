cfea05fe0116a19199cec35a14dcda8ee5728d04
kurosio <18047352+kurosio@users.noreply.github.com>
2024-10-30 01:59:37 +0700

    rm dbg

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -485,8 +485,6 @@ void CPlayer::TryRespawn()
 		GS()->CreatePlayerSpawn(SpawnPos);
 		GetTempData().ClearTeleportPosition();
 		m_WantSpawn = false;
-
-		GS()->SendMenuMotd(this, MOTD_MENU_WIKI_INFO);
 	}
 }
 
