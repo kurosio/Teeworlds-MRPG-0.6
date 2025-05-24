7b058b0a37e5fafa044c3ebcf8104ed4cdd2be8b
Vladislav <harborwolf1@gmail.com>
2025-05-24 23:04:55 +0700

    missed

--- src/game/server/core/components/skills/entities/attack_teleport/attack_teleport.cpp
+++ src/game/server/core/components/skills/entities/attack_teleport/attack_teleport.cpp
@@ -99,7 +99,7 @@ void CAttackTeleport::Tick()
 				CPlayer* pNextPlayer = nullptr;
 				CCharacter* pNextChar = nullptr;
 				const auto currentDmgSize = m_pPlayer->GetTotalAttributeValue(AttributeIdentifier::DMG);
-				const int maxDmgSize = maximum(1, translate_to_percent_rest(currentDmgSize, clamp(m_SkillBonus, 5, 50)));
+				const int maxDmgSize = maximum(1, translate_to_percent_rest(currentDmgSize, clamp(m_SkillBonus, 1, 30)));
 
 				// try get next player
 				while(!pNextPlayer && !pNextChar && !m_vMovingMap.empty())
