d7e6071322756a9e0d8672e2ec172e5bd478881d
Vladislav <harborwolf1@gmail.com>
2025-05-31 19:11:59 +0700

    missed for checking valid item

--- src/game/server/core/components/accounts/equipped_slots.cpp
+++ src/game/server/core/components/accounts/equipped_slots.cpp
@@ -38,7 +38,7 @@ void EquippedSlots::load(const std::string& equippedSlotsJson)
 
 			if(jsonItemID.is_number_integer())
 			{
-				CItem item(jsonItemID.get<int>());
+				CItem item(jsonItemID.get<int>(), 1);
 				it->second = (item.IsValid() && item.Info()->IsType(currentItemType) ? std::make_optional<int>(item.GetID()) : std::nullopt);
 			}
 			else if(jsonItemID.is_null())
