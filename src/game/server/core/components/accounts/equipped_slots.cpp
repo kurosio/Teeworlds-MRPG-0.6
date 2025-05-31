2c9721a54776a320dc6bcce077f030cd24476dc1
Vladislav <harborwolf1@gmail.com>
2025-05-31 17:58:19 +0700

    equipped slots structure from map to vector

--- src/game/server/core/components/accounts/equipped_slots.cpp
+++ src/game/server/core/components/accounts/equipped_slots.cpp
@@ -4,68 +4,90 @@
 
 void EquippedSlots::initSlot(ItemType type, std::optional<int> itemID)
 {
-	m_Slots[type] = itemID;
+	auto it = findSlotIter(m_Slots, type);
+	if(it != m_Slots.end())
+		it->second = itemID;
+	else
+		m_Slots.emplace_back(type, itemID);
 }
 
-void EquippedSlots::load(const std::string& EquippedSlots)
+bool EquippedSlots::hasSlot(ItemType type) const
 {
-	// loading from data
-	mystd::json::parse(EquippedSlots, [this](nlohmann::json& json)
+	return findSlotIter(m_Slots, type) != m_Slots.end();
+}
+
+std::optional<int> EquippedSlots::getEquippedItemID(ItemType type) const
+{
+	auto it = findSlotIter(m_Slots, type);
+	return it != m_Slots.end() ? it->second : std::nullopt;
+}
+
+void EquippedSlots::load(const std::string& equippedSlotsJson)
+{
+	mystd::json::parse(equippedSlotsJson, [this](nlohmann::json& jsonData)
 	{
-		for(auto& [T, ItemID] : json.items())
+		if(!jsonData.is_object())
+			return;
+
+		for(auto& [strType, jsonItemID] : jsonData.items())
 		{
-			auto Type = (ItemType)std::stoi(T);
-			if(m_Slots.contains(Type))
+			const auto currentItemType = (ItemType)std::stoi(strType);
+			auto it = findSlotIter(m_Slots, currentItemType);
+			if(it == m_Slots.end())
+				continue;
+
+			if(jsonItemID.is_number_integer())
 			{
-				auto ResultOpt = (ItemID > 0 ? std::make_optional<int>(ItemID) : std::nullopt);
-				m_Slots[Type] = ResultOpt;
+				CItem item(jsonItemID.get<int>());
+				it->second = (item.IsValid() && !item.Info()->IsType(currentItemType) ? std::make_optional<int>(item.GetID()) : std::nullopt);
+			}
+			else if(jsonItemID.is_null())
+			{
+				it->second = std::nullopt;
 			}
 		}
 	});
 }
 
-bool EquippedSlots::equipItem(int ItemID)
+bool EquippedSlots::equipItem(int itemID)
 {
-	if(!CItemDescription::Data().contains(ItemID))
+	if(!CItemDescription::Data().contains(itemID))
 		return false;
 
-	auto* pGS = (CGS*)Instance::GameServer();
-	auto* pItemInfo = pGS->GetItemInfo(ItemID);
-	if(!m_Slots.contains(pItemInfo->GetType()))
+	const auto* pGS = (CGS*)Instance::GameServer();
+	const auto* pItemInfo = pGS->GetItemInfo(itemID);
+	const auto typeToEquip = pItemInfo->GetType();
+	auto it = findSlotIter(m_Slots, typeToEquip);
+
+	if(it == m_Slots.end() || (it->second.has_value() && it->second.value() == itemID))
 		return false;
 
-	if(m_Slots[pItemInfo->GetType()] == ItemID)
-		return false;
-
-	m_Slots[pItemInfo->GetType()] = ItemID;
+	it->second = itemID;
 	return true;
 }
 
-bool EquippedSlots::unequipItem(int ItemID)
+bool EquippedSlots::unequipItem(int itemID)
 {
-	if(!CItemDescription::Data().contains(ItemID))
+	if(!CItemDescription::Data().contains(itemID))
 		return false;
 
 	auto* pGS = (CGS*)Instance::GameServer();
-	auto* pItemInfo = pGS->GetItemInfo(ItemID);
-	if(!m_Slots.contains(pItemInfo->GetType()))
+	auto* pItemInfo = pGS->GetItemInfo(itemID);
+	const auto typeToUnequip = pItemInfo->GetType();
+	auto it = findSlotIter(m_Slots, typeToUnequip);
+
+	if(it == m_Slots.end() || !it->second.has_value() || it->second.value() != itemID)
 		return false;
 
-	if(m_Slots[pItemInfo->GetType()] != ItemID)
-		return false;
-
-	m_Slots[pItemInfo->GetType()] = std::nullopt;
+	it->second = std::nullopt;
 	return true;
 }
 
 nlohmann::json EquippedSlots::dumpJson() const
 {
-    nlohmann::json js {};
+	nlohmann::json js = nlohmann::json::object();
+	for(const auto& [slotTypeId, slotItemId] : m_Slots)
+		js[std::to_string((int)slotTypeId)] = slotItemId.value_or(NOPE);
 
-	for(auto& [Type, Value] : m_Slots)
-	{
-		js[std::to_string((int)Type)] = Value.value_or(-1);
-	}
-
-    return js;
+	return js;
 }
--- src/game/server/core/components/accounts/equipped_slots.h
+++ src/game/server/core/components/accounts/equipped_slots.h
@@ -6,33 +6,33 @@ class CAccountData;
 
 class EquippedSlots
 {
-	std::unordered_map<ItemType, std::optional<int>> m_Slots {};
-
 public:
+	using SlotEntry = std::pair<ItemType, std::optional<int>>;
 	EquippedSlots() = default;
 
 	void initSlot(ItemType type, std::optional<int> itemID = std::nullopt);
+	bool equipItem(int itemID);
+	bool unequipItem(int itemID);
+	void load(const std::string& equippedSlotsJson);
 
-	const std::unordered_map<ItemType, std::optional<int>>& getSlots() const
-	{
-		return m_Slots;
-	}
-
-	bool hasSlot(ItemType Slot) const
-	{
-		return m_Slots.contains(Slot);
-	}
-
-	std::optional<int> getEquippedItemID(ItemType Type) const
-	{
-		return m_Slots.contains(Type) ? m_Slots.at(Type) : std::nullopt;
-	}
-
-	bool equipItem(int ItemID);
-	bool unequipItem(int ItemID);
-	void load(const std::string& EquippedSlots);
-
+	const std::vector<SlotEntry>& getSlots() const { return m_Slots; }
+	bool hasSlot(ItemType type) const;
+	std::optional<int> getEquippedItemID(ItemType type) const;
 	nlohmann::json dumpJson() const;
+
+private:
+	std::vector<SlotEntry> m_Slots {};
+	auto findSlotIter(const std::vector<EquippedSlots::SlotEntry>& slots, ItemType type) const
+	{
+		return std::ranges::find_if(slots, [type](const auto& entry)
+			{ return entry.first == type; });
+	}
+
+	auto findSlotIter(std::vector<EquippedSlots::SlotEntry>& slots, ItemType type)
+	{
+		return std::ranges::find_if(slots, [type](const auto& entry)
+			{ return entry.first == type; });
+	}
 };
 
 #endif
