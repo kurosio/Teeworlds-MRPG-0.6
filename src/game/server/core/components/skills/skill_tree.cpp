4c64dcce726cf9a49e4aaa07f4fc5ebb2f0235c9
Vladislav <harborwolf1@gmail.com>
2026-06-07 19:47:13 +0700

    refactoring skill tree

--- src/game/server/core/components/skills/skill_tree.cpp
+++ src/game/server/core/components/skills/skill_tree.cpp
@@ -1,3 +1,118 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
+
 #include "skill_tree.h"
+
+std::string format_skill_modifier(SkillMod Mod, int Value)
+{
+	switch(Mod)
+	{
+		case SkillMod::BonusIncreaseValue:
+		case SkillMod::Radius:
+			return fmt_default(" +{}", Value);
+
+		case SkillMod::BonusDecreaseValue:
+		case SkillMod::CastClick:
+			return fmt_default(" -{}", Value);
+
+		case SkillMod::BonusDecreasePct:
+		case SkillMod::ManaCostPct:
+			return fmt_default(" -{}%", Value);
+
+		case SkillMod::BonusIncreasePct:
+		case SkillMod::MasterCraftExtraItemPct:
+			return fmt_default(" +{}%", Value);
+
+		case SkillMod::Lifetime:
+			return fmt_default(" +{}sec", Value);
+
+		case SkillMod::None:
+		case SkillMod::AttackTeleportCombo:
+		case SkillMod::AttackTeleportStun:
+		case SkillMod::AttackTeleportFire:
+		case SkillMod::AttackTeleportRestoreHP:
+		case SkillMod::CureAlly:
+		case SkillMod::MasterWeaponAutoFire:
+		default:
+			return "";
+	}
+}
+
+const CSkillTreeOption* CSkillTreeLevel::FindOption(int OptionIdx) const
+{
+	const auto It = Options.find(OptionIdx);
+	return It != Options.end() ? &It->second : nullptr;
+}
+
+bool CSkillTreeLevel::HasOption(int OptionIdx) const
+{
+	return Options.contains(OptionIdx);
+}
+
+std::size_t CSkillTreeLevel::GetOptionsCount() const noexcept
+{
+	return Options.size();
+}
+
+void CSkillTree::LoadFromDB()
+{
+	auto& Data = CSkillTree::Data();
+	Data.clear();
+
+	ResultPtr pRes = Database->Execute<DB::SELECT>("*", "tw_skills_tree");
+	while(pRes->next())
+	{
+		const int SkillID = pRes->getInt("SkillID");
+		const int LevelIndex = pRes->getInt("LevelIndex");
+		const int OptionIndex = pRes->getInt("OptionIndex");
+
+		if(SkillID <= 0)
+			continue;
+
+		auto& Tree = Data[SkillID];
+		Tree.SkillID = SkillID;
+
+		if(LevelIndex <= 0 || OptionIndex <= 0)
+			continue;
+
+		auto& Opt = Tree.Levels[LevelIndex].Options[OptionIndex];
+		Opt.Name = pRes->getString("Name");
+		Opt.Description = pRes->getString("Description");
+		Opt.ModType = static_cast<SkillMod>(pRes->getInt("ModType"));
+		Opt.ModValue = pRes->getInt("ModValue");
+		Opt.PriceSP = pRes->getInt("PriceSP");
+	}
+}
+
+const CSkillTree* CSkillTree::Get(int ID)
+{
+	const auto& Data = CSkillTree::Data();
+	const auto It = Data.find(ID);
+	return It != Data.end() ? &It->second : nullptr;
+}
+
+const CSkillTreeLevel* CSkillTree::GetLevel(int LevelIdx) const
+{
+	const auto It = Levels.find(LevelIdx);
+	return It != Levels.end() ? &It->second : nullptr;
+}
+
+int CSkillTree::GetMaxLevels() const noexcept
+{
+	if(Levels.empty())
+		return 0;
+
+	return Levels.rbegin()->first;
+}
+
+bool CSkillTree::HasOption(int LevelIdx, int OptionIdx) const
+{
+	const auto* pLevel = GetLevel(LevelIdx);
+	return pLevel != nullptr && pLevel->HasOption(OptionIdx);
+}
+
+const CSkillTreeOption* CSkillTree::FindOption(int LevelIdx, int OptionIdx) const
+{
+	const auto* pLevel = GetLevel(LevelIdx);
+	return pLevel != nullptr ? pLevel->FindOption(OptionIdx) : nullptr;
+}
--- src/game/server/core/components/skills/skill_tree.h
+++ src/game/server/core/components/skills/skill_tree.h
@@ -19,51 +19,25 @@ enum class SkillMod : int
 	MasterCraftExtraItemPct = 10,
 
 	// Attack teleport
-	AttackTeleportCombo = 11,       // Allow combo attack
-	AttackTeleportStun = 12,        // Attack teleport has stun effect
-	AttackTeleportFire = 13,        // Attack teleport has fire effect
-	AttackTeleportRestoreHP = 14,   // Attack teleport can restore health
+	AttackTeleportCombo = 11,      // Allow combo attack
+	AttackTeleportStun = 12,       // Attack teleport has stun effect
+	AttackTeleportFire = 13,       // Attack teleport has fire effect
+	AttackTeleportRestoreHP = 14,  // Attack teleport can restore health
 
 	// Cure
-	CureAlly = 15,                  // Cure can restore health for ally
+	CureAlly = 15,                 // Cure can restore health for ally
 
 	// Master weapon
-	MasterWeaponAutoFire = 16,      // Auto fire from weapons
+	MasterWeaponAutoFire = 16,     // Auto fire from weapons
 };
 
-inline std::string format_skill_modifier(SkillMod Mod, int Value)
-{
-	switch(Mod)
-	{
-		default:
-			return "";
-
-		case SkillMod::BonusIncreaseValue:
-		case SkillMod::Radius:
-			return fmt_default(" +{}", Value);
-
-		case SkillMod::BonusDecreaseValue:
-		case SkillMod::CastClick:
-			return fmt_default(" -{}", Value);
-
-		case SkillMod::BonusDecreasePct:
-		case SkillMod::ManaCostPct:
-			return fmt_default(" -{}%", Value);
-
-		case SkillMod::BonusIncreasePct:
-		case SkillMod::MasterCraftExtraItemPct:
-			return fmt_default(" +{}%", Value);
-
-		case SkillMod::Lifetime:
-			return fmt_default(" +{}sec", Value);
-	}
-}
+[[nodiscard]] std::string format_skill_modifier(SkillMod Mod, int Value);
 
 struct CSkillTreeOption
 {
-	std::string Name {};
-	std::string Description {};
-	SkillMod ModType = SkillMod::None;
+	std::string Name;
+	std::string Description;
+	SkillMod ModType { SkillMod::None };
 	int ModValue {};
 	int PriceSP {};
 };
@@ -72,76 +46,23 @@ struct CSkillTreeLevel
 {
 	std::map<int, CSkillTreeOption> Options;
 
-	const CSkillTreeOption* FindOption(int OptionIdx) const
-	{
-		if(const auto it = Options.find(OptionIdx); it != Options.end())
-			return &it->second;
-		return nullptr;
-	}
-
-	bool HasOption(int OptionIdx) const { return Options.contains(OptionIdx); }
-	size_t GetOptionsCount() const { return Options.size(); }
+	[[nodiscard]] const CSkillTreeOption* FindOption(int OptionIdx) const;
+	[[nodiscard]] bool HasOption(int OptionIdx) const;
+	[[nodiscard]] std::size_t GetOptionsCount() const noexcept;
 };
 
-struct CSkillTree : public MultiworldIdentifiableData< std::unordered_map < int, CSkillTree > >
+struct CSkillTree : public MultiworldIdentifiableData<std::unordered_map<int, CSkillTree>>
 {
 	int SkillID {};
-	std::map<int, CSkillTreeLevel> Levels {};
+	std::map<int, CSkillTreeLevel> Levels;
 
-	static void LoadFromDB()
-	{
-		m_pData.clear();
+	static void LoadFromDB();
 
-		ResultPtr pRes = Database->Execute<DB::SELECT>("*", "tw_skills_tree");
-		while(pRes->next())
-		{
-			const int SkillID = pRes->getInt("SkillID");
-			const int LevelIndex = pRes->getInt("LevelIndex");
-			const int OptionIndex = pRes->getInt("OptionIndex");
-
-			auto& Tree = m_pData[SkillID];
-			Tree.SkillID = SkillID;
-			if(LevelIndex <= 0 || OptionIndex <= 0)
-				continue;
-
-			auto& Opt = Tree.Levels[LevelIndex].Options[OptionIndex];
-			Opt.Name = pRes->getString("Name");
-			Opt.Description = pRes->getString("Description");
-			Opt.ModType = static_cast<SkillMod>(pRes->getInt("ModType"));
-			Opt.ModValue = pRes->getInt("ModValue");
-			Opt.PriceSP = pRes->getInt("PriceSP");
-		}
-	}
-
-	static CSkillTree* Get(int ID)
-	{
-		if(const auto it = CSkillTree::Data().find(ID); it != CSkillTree::Data().end())
-			return &it->second;
-		return nullptr;
-	}
-
-	const CSkillTreeLevel* GetLevel(int LevelIdx) const
-	{
-		if(const auto it = Levels.find(LevelIdx); it != Levels.end())
-			return &it->second;
-		return nullptr;
-	}
-
-	int GetMaxLevels() const { return Levels.size(); }
-
-	bool HasOption(int LevelIdx, int OptionIdx) const
-	{
-		if(const auto* pLevel = GetLevel(LevelIdx))
-			return pLevel->HasOption(OptionIdx);
-		return false;
-	}
-
-	const CSkillTreeOption* FindOption(int LevelIdx, int OptionIdx) const
-	{
-		if(const auto* pLevel = GetLevel(LevelIdx))
-			return pLevel->FindOption(OptionIdx);
-		return nullptr;
-	}
+	[[nodiscard]] static const CSkillTree* Get(int ID);
+	[[nodiscard]] const CSkillTreeLevel* GetLevel(int LevelIdx) const;
+	[[nodiscard]] int GetMaxLevels() const noexcept;
+	[[nodiscard]] bool HasOption(int LevelIdx, int OptionIdx) const;
+	[[nodiscard]] const CSkillTreeOption* FindOption(int LevelIdx, int OptionIdx) const;
 };
 
-#endif
+#endif // GAME_SERVER_CORE_COMPONENTS_SKILLS_SKILL_TREE_H
